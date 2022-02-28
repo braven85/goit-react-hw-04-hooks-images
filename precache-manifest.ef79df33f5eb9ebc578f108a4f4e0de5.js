a0d9252fc80aab6bc8d54b279b0bfafe2614d680
Radek <radekie@wp.pl>
2022-02-28 21:46:29 +0100

    Updates

---
+++ precache-manifest.ef79df33f5eb9ebc578f108a4f4e0de5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "554168f45edcbc6de2ba",
+    "url": "/goit-react-hw-04-hooks-images/static/css/main.833c81d0.chunk.css"
+  },
+  {
+    "revision": "554168f45edcbc6de2ba",
+    "url": "/goit-react-hw-04-hooks-images/static/js/main.554168f4.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/goit-react-hw-04-hooks-images/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "ec45592468da8d36763a",
+    "url": "/goit-react-hw-04-hooks-images/static/css/2.40f1849d.chunk.css"
+  },
+  {
+    "revision": "ec45592468da8d36763a",
+    "url": "/goit-react-hw-04-hooks-images/static/js/2.ec455924.chunk.js"
+  },
+  {
+    "revision": "505198e5386922d90ca8",
+    "url": "/goit-react-hw-04-hooks-images/static/js/runtime~main.505198e5.js"
+  },
+  {
+    "revision": "929602c920696889727d22d6ad81daca",
+    "url": "/goit-react-hw-04-hooks-images/static/media/search.929602c9.svg"
+  },
+  {
+    "revision": "1ec090b865d6b1c0b8b01a9df3c5fc10",
+    "url": "/goit-react-hw-04-hooks-images/index.html"
+  }
+];
